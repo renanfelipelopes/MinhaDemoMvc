8bb3e1fabd5a4f230dfdb47b6ca04c99615e6750
Renan Lopes <renanlopes.ads@gmail.com>
2021-11-28 17:21:51 -0300

    Rotas Inteligentes

--- MinhaDemoMvc/Controllers/HomeController.cs
+++ MinhaDemoMvc/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 
 namespace MinhaDemoMvc.Controllers
 {
+    [Route("")]
+    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -18,17 +20,23 @@ namespace MinhaDemoMvc.Controllers
             _logger = logger;
         }
 
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
         public IActionResult Index(string id, string categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
